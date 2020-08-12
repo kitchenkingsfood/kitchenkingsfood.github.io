0087b40fcb1951a8819ba36305eec6868699dc78
banelekhumalo <67458707+banelekhumalo@users.noreply.github.com>
2020-08-12 21:43:52 +0200

    edited

---
+++ main.js
@@ -0,0 +1,11 @@
+/*Slider*/
+$(".slider-two")
+.not(".slick-initializes")
+.slick({
+   
+    prevArrow:" site.slider-two .prev",
+    nextArrow:".site-slider.two .next",
+    slidesToShow:5,
+    slidesToScroll:1,
+    autoplaySpeed:3000,
+});
